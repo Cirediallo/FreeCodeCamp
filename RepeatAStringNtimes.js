de3585d7820ce9fa3799396d5c6500b100b2c326
Cirediallo <theevilmonster93@gmail.com>
2019-04-04 20:49:46 +0200

    Create RepeatAStringNtimes.js

---
+++ RepeatAStringNtimes.js
@@ -0,0 +1,9 @@
+function repeatStringNumTimes(str, n) {
+  // repeat after me
+  /*
+  if(num < 0){return "";}
+  let i = 1; let s = str
+  while(i < num){str += s; i++; }
+  */
+  return num > 0 ? str.repeat(num) : '';
+}
