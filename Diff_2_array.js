6299950688d899d488b003e7faeb0fe688c50f4b
Cirediallo <theevilmonster93@gmail.com>
2019-05-28 14:34:32 +0200

    Update Diff_2_array.js

--- Diff_2_array.js
+++ Diff_2_array.js
@@ -1,7 +1,6 @@
 function diffArray(arr1, arr2) {
   var newArr = [...arr1,...arr2];
-  let diff = newArr.filter((elt) => {
+  return newArr.filter((elt) => {
     return !(arr1.includes(elt) && arr2.includes(elt));
   });
-  return diff;
 }
