67088309f4cdafb6687d24da62371a354323115e
Cirediallo <theevilmonster93@gmail.com>
2019-07-22 00:48:17 +0200

    Create ReduxCopyObjectWithObjectassign.js

---
+++ ReduxCopyObjectWithObjectassign.js
@@ -0,0 +1,26 @@
+const defaultState = {
+  user: 'CamperBot',
+  status: 'offline',
+  friends: '732,982',
+  community: 'freeCodeCamp'
+};
+
+const immutableReducer = (state = defaultState, action) => {
+  switch(action.type) {
+    case 'ONLINE':
+      // don't mutate state here or the tests will fail
+      let newO = Object.assign({}, state)
+      newO.status = 'online'
+      return newO
+    default:
+      return state;
+  }
+};
+
+const wakeUp = () => {
+  return {
+    type: 'ONLINE'
+  }
+};
+
+const store = Redux.createStore(immutableReducer);
