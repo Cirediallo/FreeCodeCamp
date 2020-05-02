1db33e434ccda2c8aaf9005dc727d708f6f4f773
Cirediallo <theevilmonster93@gmail.com>
2020-05-02 16:46:21 +0200

    Update booWho.js

--- booWho.js
+++ booWho.js
@@ -1,4 +1,13 @@
 function booWho(bool) {
+  
+  if(bool === true || bool === false)
+    return true;
+  return false;
+  
+  // ========== With typeof
   if(typeof(bool) == "boolean"){return true;}
   else return false;
+  
+  // === or
+  return typeof bool === 'boolean'
 }
