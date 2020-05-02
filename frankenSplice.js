86fda06689b619c61b9efd14673b1dfa69764b98
Cirediallo <theevilmonster93@gmail.com>
2020-05-02 19:41:45 +0200

    Update frankenSplice.js
    This function insert arr1 in arr2 begining at index n

--- frankenSplice.js
+++ frankenSplice.js
@@ -1,9 +1,17 @@
 function frankenSplice(arr1, arr2, n) {
-  // It's alive. It's alive!
   let copy = [...arr2];
   for(let i= 0; i < arr1.length; i++){
     copy.splice(n,0,arr1[i]);
     n++;
   }
+  return copy;
+  
+  // ========== With splice and slice
+  let tmp = arr2.slice(arr2.length-n)
+  return arr2.slice(n-n,n).concat(arr1).concat(tmp);
+  
+  // ==== or
+  let copy = arr2.slice();
+  copy.splice(n, 0, ...arr1);
   return copy;
 }
