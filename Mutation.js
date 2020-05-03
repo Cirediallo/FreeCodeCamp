7f602d1706d42eeee6c7d73a55e45d2e7099b1a0
Cirediallo <theevilmonster93@gmail.com>
2020-05-03 19:03:15 +0200

    Update Mutation.js
    This function check if the string in the first element of the array contains all the letters of the string in the second element of the array and return true if yes or false otherwise

--- Mutation.js
+++ Mutation.js
@@ -1,8 +1,15 @@
 function mutation(arr) {
+  // ========== Naïve Procedural way
   let t1 = arr[0].toLowerCase();
   let t2 = arr[1].toLowerCase();
   for(let i = 0; i < t2.length; i++){
     if(t1.indexOf(t2[i]) < 0){return false;}
   }
   return true;
+  
+  // ========== Declarative way with split and every combined to indexOf
+  return arr[1].toLowerCase().split("").every(function(elt){
+    console.log(elt)
+    return arr[0].toLowerCase().indexOf(elt) != -1
+  })
 }
