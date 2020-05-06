d7a83131e58ae241b239d4240af4ecfdb9f5cb67
Cirediallo <theevilmonster93@gmail.com>
2020-05-06 16:05:43 +0200

    Update ReturnSortedArrayWithoutChangingTheOriginal.js

--- ReturnSortedArrayWithoutChangingTheOriginal.js
+++ ReturnSortedArrayWithoutChangingTheOriginal.js
@@ -1,3 +1,9 @@
 nonMutatingSortd(arr) {
+  // ========== With sort
+  return [...arr].sort((a,b) => {
+    return a === b ? 0 : a < b ? -1 : 1;
+  });
+  
+  // ========== With concat and sort
   return [].concat(arr).sort((a,b) => a -b)
 }
