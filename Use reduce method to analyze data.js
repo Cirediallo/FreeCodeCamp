0c674563885dd159c0b3ede313eb3bd9f6c1acdf
Cirediallo <theevilmonster93@gmail.com>
2020-05-06 12:49:14 +0200

    Update Use reduce method to analyze data.js

--- Use reduce method to analyze data.js
+++ Use reduce method to analyze data.js
@@ -1,3 +1,18 @@
-let total = watchList.filter(elt => elt.Director === "Christopher Nolan").map(obj => parseFloat(obj.imdbRating)).reduce((a,b) => a +b);
+function getRating(watchList){
+  // ========== 1st Way
+  let averageRating;
+  let tmp = watchList.map(elt => ({
+    director: elt["Director"],
+    imdbRating: elt["imdbRating"]
+  })).filter(elt => elt["director"] === "Christopher Nolan")
 
-var averageRating =  total / watchList.filter(elt => elt.Director === "Christopher Nolan").length;
+  averageRating = tmp.map(elt => parseFloat(elt["imdbRating"])).reduce((acc, curr) => (acc + curr) )
+  return averageRating/tmp.length;
+  
+  // ========== 2nd Way
+  let total = watchList.filter(elt => elt.Director === "Christopher Nolan")
+                       .map(obj => parseFloat(obj.imdbRating)).reduce((a,b) => a +b);
+
+  let averageRating =  total / watchList.filter(elt => elt.Director === "Christopher Nolan").length;
+
+}
