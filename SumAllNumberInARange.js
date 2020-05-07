44b6529cdec31b398183a9ad121f8ec1330a5e6c
Cirediallo <theevilmonster93@gmail.com>
2020-05-08 01:18:40 +0200

    Update SumAllNumberInARange.js
    this function return the sum of all numbers in a range passed as an array.
    Recall that the array must contain only two elements

--- SumAllNumberInARange.js
+++ SumAllNumberInARange.js
@@ -1,23 +1,24 @@
 function sumAll(arr){
+  // ========== Naïve Way 
+  let sorted = arr.sort( function(a,b){ return a - b });
+  let result = 0;
+  for(let i = sorted[0]; i <= sorted[sorted.length-1]; i++){
+    result += i;
+  }
+  return result;
+  
+  // ========== With Math.min and Math.max
   let sum = 0;
   for(let i = Math.min(...arr); i <= Math.max(...arr); i++){
     sum += i;
   }
   return sum;
-}
   
-  /*
-    ==== SOLUTION FROM FCC
-  {
-    // Using ES6 arrow function (one-liner)
-    var sortedArr = arr.sort((a,b) => a-b);
-    var firstNum = arr[0];
-    var lastNum = arr[1];
-    // Using Arithmetic Progression summing formula
+  // ========== With Arithmetic Progression summing formula
+  const startNum = arr[0];
+  const endNum = arr[1];
+  const numberElementBetweenCount = Math.abs(startNum - endNum) + 1; //between 1 and 4 => |1-4| + 1 = 4, (1, 2, 3, 4), <=> 4 numbers
 
-    var sum = (lastNum - firstNum + 1) * (firstNum + lastNum) / 2;
-    return sum;
-  },
+  const sum = ((startNum + endNum) * numberElementBetweenCount) / 2; // Arithmetic Progression summing
+  return sum;
   
-    
-  */
