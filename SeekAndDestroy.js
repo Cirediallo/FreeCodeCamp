3913f2e4237995d8db5885a06fc29243202b119e
Cirediallo <theevilmonster93@gmail.com>
2020-05-09 19:43:45 +0200

    Update SeekAndDestroy.js

--- SeekAndDestroy.js
+++ SeekAndDestroy.js
@@ -1,4 +1,22 @@
 function destroyer(arr){
+  // ========== Naïve way
+  
+  let args = Array.from(arguments);
+  
+  for(let i = 0; i < arguments.length; i++){
+    for(let j = 0; j < arr.length; j++){
+      if(arguments[i] === arr[j]){delete arr[j]}
+    }
+  }
+  let destroyed = arr.filter(
+      elt => {
+        if(elt){return elt}
+      }
+    )
+  return destroyed;
+  
+  
+  // ========== using delete
   for(let i = 0; i < arguments.length; i++){
     for(let j = 0; j < arr.length; j++){
       if(argument[i] === arr[i]){delete arr[i];}
@@ -6,6 +24,7 @@ function destroyer(arr){
   }
   return arr.filter(Boolean);
 }
+
 
 /* ===== ADVANCED SOLUTION ====== */
 
