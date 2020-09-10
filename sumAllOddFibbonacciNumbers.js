cc3ff4786cfa3e3112a68ab5aea6ac0deca1208d
Cirediallo <theevilmonster93@gmail.com>
2020-09-10 22:07:45 +0200

    Create sumAllOddFibbonacciNumbers.js
    This function compute and return the sum of all odd Fibonacci numbers that are less than or equal to the given parameter

---
+++ sumAllOddFibbonacciNumbers.js
@@ -0,0 +1,19 @@
+function sumFibs(num) {
+  let fibbo = [1,1];
+  let curr = fibbo[1], prev = fibbo[0];
+  let oddSum =0, i = 1, next = 0;
+  while(true){
+    next = prev + curr;
+    fibbo.push(next);
+    prev = fibbo[i];
+    i++;
+    curr = fibbo[i];
+    if(next > num){break;}
+  }
+  fibbo.map(elt => {
+    if(elt % 2 !== 0 && elt <= num){
+      oddSum += elt;
+    }
+  });
+  return oddSum;
+}
